5d34a82fd509aed4e45aa36b4655b5a72b76a507
Golam Fazle Rabbi <golam.r@Golams-MacBook-Pro.local>
2022-11-13 03:32:17 +0700

    project type added

--- graphql_server/server/schema/schema.js
+++ graphql_server/server/schema/schema.js
@@ -1,5 +1,5 @@
-const { Projects, clients } = require('../sampleData');
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');
+const { projects, clients } = require('../sampleData');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
 
 const ClientType = new GraphQLObjectType({
     name: 'Client',
@@ -11,9 +11,37 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString }
+    })
+});
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return projects.find(project => project.id === args.id);
+            }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            }
+        },
         client: {
             type: ClientType,
             args: {id: {type: GraphQLID}},
